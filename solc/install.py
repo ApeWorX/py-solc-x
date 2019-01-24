9d21424aa7ad327b586dd94a7e8b4eaeb5e95274
iamdefinitelyahuman <ben.hauser@hyperlink.capital>
2019-01-24 10:33:45 +0200

    better version handling, reorg

--- solc/install.py
+++ solc/install.py
@@ -14,7 +14,7 @@ import zipfile
 DOWNLOAD_BASE = "https://github.com/ethereum/solidity/releases/download/{}/{}"
 API = "https://api.github.com/repos/ethereum/solidity/releases/latest"
 
-sep = "\\" if    sys.platform == "win32" else "/"
+sep = "\\" if sys.platform == "win32" else "/"
 solc_version = None
 
 
@@ -46,18 +46,31 @@ def get_installed_solc_versions():
 def install_solc(version = None):
     version = _check_version(version)
     if sys.platform.startswith('linux'):
-        return _install_solc_linux(version)
+        _install_solc_linux(version)
     elif sys.platform == 'darwin':
-        return _install_solc_osx(version)
+        _install_solc_osx(version)
     elif sys.platform == 'win32':
-        return _install_solc_windows(version)
-    raise KeyError("Unknown platform: {}".format(sys.platform))
+        _install_solc_windows(version)
+    else:
+        raise KeyError("Unknown platform: {}".format(sys.platform))
+    binary_path = get_executable(version)
+    _check_subprocess_call(
+        [binary_path, '--version'],
+        message="Checking installed executable version @ {}".format(binary_path)
+    )
+    print("solc {} successfully installed at: {}".format(version, binary_path))
 
 
 def _check_version(version):
     if not version:
         return requests.get(API).json()['tag_name']
-    return "v0." + version.lstrip("v0.")
+    version = "v0." + version.lstrip("v0.")
+    if version.count('.') != 2:
+        raise ValueError("solc version must be in the format v0.x.x")
+    v = [int(i) for i in version[1:].split('.')]
+    if v[1] < 4 or (v[1] == 4 and v[2] < 11):
+        raise ValueError("py-solc-x does not support solc versions <0.4.11")
+    return version
 
 
 def _check_subprocess_call(command, message=None, verbose=True, **proc_kwargs):
@@ -77,30 +90,39 @@ def _chmod_plus_x(executable_path):
     os.chmod(executable_path, current_st.st_mode | stat.S_IEXEC)
 
 
+def _wget(url, path):
+    try:
+        _check_subprocess_call(
+            ["wget", url, "-O", path],
+            "Downloading solc from {}".format(url)
+        )
+    except subprocess.CalledProcessError:
+        if os.path.exists(path):
+            os.remove(path)
+        raise
+
+
 def _install_solc_linux(version):
     download = DOWNLOAD_BASE.format(version, "solc-static-linux")
     binary_path = get_solc_folder()+"solc-{}".format(version)
     if os.path.exists(binary_path):
         print("solc {} already installed at: {}".format(version, binary_path))
         return
-
-    _check_subprocess_call(
-        [
-            "wget", download,
-            '-c',  # resume previously incomplete download.
-            '-O', binary_path,
-        ],
-        message="Downloading static linux binary from {}".format(download)
-    )
-
+    _wget(download, binary_path)
     _chmod_plus_x(binary_path)
 
-    _check_subprocess_call(
-        [binary_path, '--version'],
-        message="Checking installed executable version @ {}".format(binary_path)
-    )
 
-    print("solc {} successfully installed at: {}".format(version, binary_path))
+def _install_solc_windows(version):
+    download = DOWNLOAD_BASE.format(version, "solidity-windows.zip")
+    zip_path = get_solc_folder() + 'solc_{}.zip'.format(version[1:])
+    install_folder = get_solc_folder()+"solc-{}".format(version)
+    if os.path.exists(install_folder):
+        print("solc {} already installed at: {}".format(version, install_folder))
+        return
+    print("Downloading solc {} from {}".format(version, download))
+    request = requests.get(download)
+    with zipfile.ZipFile(BytesIO(request.content)) as zf:
+        zf.extractall(install_folder)
 
 
 def _install_solc_osx(version):
@@ -112,19 +134,13 @@ def _install_solc_osx(version):
     if os.path.exists(binary_path):
         print("solc {} already installed at: {}".format(version, binary_path))
         return
-    
-    _check_subprocess_call(
-        [
-            "wget", download,
-            '-c',  # resume previously incomplete download.
-            '-O', tar_path,
-        ],
-        message="Downloading source from {}".format(download)
-    )
+
+    _wget(download, tar_path)
 
     with tarfile.open(tar_path, "w:gz") as tar:
         tar.extractall()
     os.remove(tar_path)
+    
     _check_subprocess_call(
         ["sh", source_folder+'/scripts/install_deps.sh'],
         message="Running dependency installation script `install_deps.sh` @ {}".format(tar_path)
@@ -142,35 +158,6 @@ def _install_solc_osx(version):
     shutil.rmtree(source_folder)
 
     _chmod_plus_x(binary_path)
-
-    _check_subprocess_call(
-        [binary_path, '--version'],
-        message="Checking installed executable version @ {}".format(binary_path)
-    )
-
-    print("solc {} successfully installed at: {}".format(version, binary_path))
-
-
-def _install_solc_windows(version):
-    download = DOWNLOAD_BASE.format(version, "solidity-windows.zip")
-    zip_path = get_solc_folder() + 'solc_{}.zip'.format(version[1:])
-    install_folder = get_solc_folder()+"solc-{}".format(version)
-    if os.path.exists(install_folder):
-        print("solc {} already installed at: {}".format(version, install_folder))
-        return
-
-    print("Downloading solc {} from {}".format(version, download))
-    request = requests.get(download)
-    with zipfile.ZipFile(BytesIO(request.content)) as zf:
-        zf.extractall(install_folder)
-    
-    binary_path = get_executable(version)
-    _check_subprocess_call(
-        [binary_path, '--version'],
-        message="Checking installed executable version @ {}".format(binary_path)
-    )
-
-    print("solc {} successfully installed at: {}".format(version, binary_path))
 
 
 if __name__ == "__main__":
