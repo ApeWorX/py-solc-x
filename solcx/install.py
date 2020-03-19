ae79f0df501e520a4721d5e1d5f3be5b2586c7ef
p-lucero <p-lucero@users.noreply.github.com>
2020-03-19 11:39:12 -0600

    Restrip v before creating Version object

--- solcx/install.py
+++ solcx/install.py
@@ -146,7 +146,7 @@ def set_solc_version_pragma(pragma_string, silent=False, check_new=False):
         LOGGER.info("Using solc version {}".format(solc_version))
     if check_new:
         latest = install_solc_pragma(pragma_string, False)
-        if Version(latest) > Version(version):
+        if Version(latest) > Version(version[1:]):
             LOGGER.info("Newer compatible solc version exists: {}".format(latest))
 
 
