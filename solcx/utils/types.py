d8c01dd07a75c6ced23f122436328345fd00ff8b
Carter Carlson <carlfarterson@gmail.com>
2020-04-28 18:45:46 -0500

    Remove methods for 1.0.0

--- solcx/utils/types.py
+++ solcx/utils/types.py
@@ -18,21 +18,9 @@ def is_string(value):
     return isinstance(value, (bytes, str, bytearray))
 
 
-def is_boolean(value):
-    return isinstance(value, bool)
-
-
 def is_dict(obj):
     return isinstance(obj, abc.Mapping)
 
 
 def is_list_like(obj):
     return not is_string(obj) and isinstance(obj, abc.Sequence)
-
-
-def is_null(obj):
-    return obj is None
-
-
-def is_number(obj):
-    return isinstance(obj, numbers.Number)
