eee13e871833ece9f9eb623740971ca700a36b8f
Edward Amor <edward.amor3@gmail.com>
2021-10-09 00:48:32 -0400

    fix: add argument to choose solc binary to `_get_combined_json_outputs`

--- solcx/main.py
+++ solcx/main.py
@@ -235,8 +235,11 @@ def compile_files(
     )
 
 
-def _get_combined_json_outputs() -> str:
-    help_str = wrapper.solc_wrapper(help=True)[0].split("\n")
+def _get_combined_json_outputs(solc_binary: Union[Path, str] = None) -> str:
+    if solc_binary is None:
+        solc_binary = get_executable()
+    
+    help_str = wrapper.solc_wrapper(solc_binary=solc_binary, help=True)[0].split("\n")
     combined_json_args = next(i for i in help_str if i.startswith("  --combined-json"))
     return combined_json_args.split(" ")[-1]
 
